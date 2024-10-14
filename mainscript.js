e062a83b959603f1fd6354e884344deb531d94ba
Jan Magister <jmagister@stud.hs-heilbronn.de>
2024-10-14 16:12:04 +0200

    adding position: sticky and better descriptions

--- mainscript.js
+++ mainscript.js
@@ -6,3 +6,97 @@ document.querySelectorAll('a').forEach(link => {
     }
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('.header');
+    const projectsTitle = document.querySelector('.container > h1:nth-of-type(1)');
+    const minecraftTitle = document.querySelector('.container > h1:nth-of-type(2)');
+
+    window.addEventListener('scroll', () => {
+        const projectsRect = projectsTitle.getBoundingClientRect();
+        const minecraftRect = minecraftTitle.getBoundingClientRect();
+        
+        // Wenn das Projects-Element aus dem sichtbaren Bereich verschwindet
+        if (projectsRect.bottom <= header.clientHeight) {
+            header.innerHTML =  `
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <a href="https://www.twitch.tv/ttvsimbagames" target="_blank">
+                                <img style="height: 60px; width: 60px; border-radius: 50%; border: 1px solid white;"
+                                    src="/images/PB.png" alt="Profilbild">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://github.com/SimbaGamess" target="_blank" rel="noopener noreferrer"
+                                style="color: white; text-decoration: none; font-size: 30px;">Jan Magister Portfolio</a>
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>
+                        </td>
+                        <td>
+                            Projects
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        `;
+        } else {
+
+            header.innerHTML = `
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <a href="https://www.twitch.tv/ttvsimbagames" target="_blank">
+                                    <img style="height: 60px; width: 60px; border-radius: 50%; border: 1px solid white;"
+                                        src="/images/PB.png" alt="Profilbild">
+                                </a>
+                            </td>
+                            <td>
+                                <a href="https://github.com/SimbaGamess" target="_blank" rel="noopener noreferrer"
+                                    style="color: white; text-decoration: none; font-size: 30px;">Jan Magister Portfolio</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            `;
+        }
+
+        // Wenn das Minecraft Data Packs-Element aus dem sichtbaren Bereich verschwindet
+        if (minecraftRect.bottom <= header.clientHeight) {
+            header.innerHTML =  `
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <a href="https://www.twitch.tv/ttvsimbagames" target="_blank">
+                                <img style="height: 60px; width: 60px; border-radius: 50%; border: 1px solid white;"
+                                    src="/images/PB.png" alt="Profilbild">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://github.com/SimbaGamess" target="_blank" rel="noopener noreferrer"
+                                style="color: white; text-decoration: none; font-size: 30px;">Jan Magister Portfolio</a>
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>
+                        </td>
+                        <td>
+                            Mincraft Data Packs
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        `;
+        }
+    });
+});
+
+
